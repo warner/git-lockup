55b6f18cd209b7657f8149f39524297c0b632298
Brian Warner <warner@lothar.com>
2012-12-05 14:28:38 -0800

    start on assembly tools
    disabled versioneer for now, not sure how it should fit

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
 from distutils.command.build_scripts import build_scripts
 
 import versioneer
 versioneer.versionfile_source = "src/_version.py"
-versioneer.versionfile_build = "src/_version.py"
+versioneer.versionfile_build = "VERSIONFILE_BUILD"
 versioneer.tag_prefix = ""
 versioneer.parentdir_prefix = "git-assure-"
 
@@ -28,9 +29,46 @@ The tools require python but no other dependencies.
 """
 
 commands = versioneer.get_cmdclass().copy()
+commands = {} # disable for now
+
+def construct(source, substitutions):
+    output = []
+    f = open(os.path.join("src", source))
+    for line in f.readlines():
+        if line.startswith("#<--"):
+            name = line.replace("#<--", "").strip()
+            if name not in substitutions:
+                print "unrecognized substitution '%s' in '%s'" % (name, source)
+                raise ValueError
+            output.append(substitutions[name])
+        else:
+            output.append(line)
+    return "".join(output)
+
 
 class my_build_scripts(build_scripts):
-    pass
+    def run(self):
+        #print "TEMPDIR", self.build_temp
+        print "BUILDDIR", self.build_dir
+        tempdir = os.path.join(self.build_dir, "temp")
+        if not os.path.isdir(tempdir):
+            os.makedirs(tempdir)
+        substitutions = {}
+        substitutions["abc"] = "abchere\n"
+
+        git_assure = os.path.join(self.build_dir, "temp", "git-assure")
+        print "creating", git_assure
+        f = open(git_assure, "w")
+        f.write(construct("git-assure-header", substitutions))
+        f.close()
+
+        # modify self.scripts with the source pathname of scripts to install
+        # into self.build_dir . When we upcall, those scripts will be copied
+        # and adjusted (their shbang line set to sys.executable).
+        self.scripts = [git_assure]
+        return build_scripts.run(self)
+
+
 
 commands["build_scripts"] = my_build_scripts
 
